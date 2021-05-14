d1cafc1a2d09dc0e711b77914943c210de683c25
Alice-Githui <gnyokabi.alice@gmail.com>
2021-05-14 15:28:58 +0300

    Add test to check the Image Model

--- personalgallery/models.py
+++ personalgallery/models.py
@@ -5,11 +5,17 @@ from django.db import models
 class Location(models.Model):
     name=models.CharField(max_length=400)
 
+    def save_location(self):
+        self.save()
+
     def __str__(self):
         return self.name
 
 class Category(models.Model):
     name=models.CharField(max_length=400)
+
+    def save_category(self):
+        self.save()
 
     def __str__(self):
         return self.name
@@ -20,6 +26,9 @@ class Image(models.Model):
     description=models.CharField(max_length=400)
     location=models.ForeignKey(Location, on_delete=models.CASCADE)
     category=models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
 
     def __str__(self):
         return self.image_name
--- personalgallery/tests.py
+++ personalgallery/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    #setup method
+
+    def setUp(self):
+
+        #creating new location and saving it
+        self.park=Location(name="park")
+        self.park.save_location()
+
+        #creating new category and saving it 
+        self.outside = Category(name="outside")
+        self.outside.save_category()
+        
+        #creating new image instance and saving it 
+        self.image1 = Image(image="https://unsplash.com/photos/8ZfTxdPvNos", image_name='Elephant Portrait', description='Gorgeous close up image of an elephant', location=self.park, category=self.outside)
+        self.image1.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image1, Image))
+
+    # def tearDown(self):
+    #     Image.objects.all().delete()
+    #     Location.objects.all().delete()
+    #     Category.objects.all.delete()
+
+
+# class LocationTestClass(TestCase):
+#     #setUpmethod
+#     def setUp(self):
+#         self.park=Park(name="park")
+#         self.park.save_location()
+
+# class CategoryTestClass(TestCase):
+#     #setUpmethod
+
+#     def setUp(self):
+#         self.outside = Category(name="outside")
+#         self.outside.save_category()
